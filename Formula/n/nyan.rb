8b3481a1523f70223e88291dccab3578ffddb2be
Toshimaru <me@toshimaru.net>
2025-07-14 09:56:22 +0900

    nyan 1.2.0 (new formula)
    - feat: Create `nyan.rb`
    - chore: Remove bottle section since it's auto-generated
    - fix: Update ldflags for the correct version output
    - test: Create a file and output the content correctly

---
+++ Formula/n/nyan.rb
@@ -0,0 +1,24 @@
+class Nyan < Formula
+  desc "Colorizing `cat` command with syntax highlighting"
+  homepage "https://github.com/toshimaru/nyan"
+  url "https://github.com/toshimaru/nyan.git",
+      tag:      "v1.2.0",
+      revision: "e586ac49a07ae7e0d30dd9d805a314ebf2231ff6"
+  license "MIT"
+  head "https://github.com/toshimaru/nyan.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/toshimaru/nyan/cmd.version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nyan -v 2>&1")
+    (testpath/"test.txt").write <<~EOS
+      nyan is a colourful cat.
+    EOS
+    assert_match(/nyan is a colourful cat./, shell_output("#{bin}/nyan test.txt"))
+  end
+end
